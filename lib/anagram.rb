f5c1a7ad1cde796f3b9f43ac65df90cbd4e9b90d
Kendan <ktoddevart@outlook.com>
2020-11-19 16:28:56 -0500

    commit tree 30d75331a488e0f0a2a3895537f241ba200679bf

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+
+    attr_accessor :anagram
+
+    def initialize(anagram)
+        @anagram = anagram
+    end
+
+    def match(words)
+        words.select do |word|
+            collect(word)
+        end
+    end
+
+    def collect(word)
+        word.chars.sort == @anagram.chars.sort
+    end
+    
+end
